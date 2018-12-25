bea5f8a35bb97f5c52d6d78db7dc2d19afb256bf
bulldozerat <bulldozerat@gmail.com>
2018-12-25 11:15:05 +0200

    Get image data name

--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -5,7 +5,13 @@ let ClassCard = (props: any) => {
         src={props.imgUrl}
         data-name={props.name}
         alt=""
+        onClick={singleCardAjax}
     />
+}
+
+function singleCardAjax(e: any) {
+    console.log(e.target.getAttribute('data-name'));
+    
 }
 
 export default ClassCard;
